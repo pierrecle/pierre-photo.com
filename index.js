20a636973f182b83c30497de27ef81955a9657c9
Pierre CLÉMENT <pierrecle@gmail.com>
2020-10-19 22:14:27 +0200

    fix index

--- index.js
+++ index.js
@@ -3,25 +3,35 @@
 const Handlebars = require("handlebars");
 const fs = require('fs');
 const gm = require('gm');
+const glob = require('glob');
 
 const baseDir = fs.realpathSync(`${__dirname}/data/themes/2019`);
 const baseOutDir = fs.realpathSync(`${__dirname}/data/website`);
 
-fs.readdirSync(`${baseDir}/partials`).forEach(file => {
-    Handlebars.registerPartial(file.replace('.hbs', ''), fs.readFileSync(`${baseDir}/partials/${file}`,  {encoding: 'utf8'}));
+const baseHelpers = glob.sync(`${__dirname}/node_modules/thumbsup/src/website/theme-base/helpers/**/*.js`);
+baseHelpers.forEach(file => {
+    const fileName = file.substring(file.lastIndexOf("/") + 1).replace('.js', '');
+    if (fileName !== "relative") {
+        Handlebars.registerHelper(fileName, require(file));
+    }
 });
 
-fs.readdirSync(`${__dirname}/node_modules/thumbsup/src/website/theme-base/helpers`).forEach(file => {
-    const jsFile = file.replace('.js', '');
-    if(jsFile !== "relative") {
-        Handlebars.registerHelper(jsFile, require(`${__dirname}/node_modules/thumbsup/src/website/theme-base/helpers/${jsFile}`));
-    }
+const themePartials = glob.sync(`${baseDir}/partials/**/*.hbs`);
+themePartials.forEach(file => {
+    const fileName = file.substring(file.lastIndexOf("/") + 1).replace('.hbs', '');
+    Handlebars.registerPartial(fileName, fs.readFileSync(file, { encoding: 'utf8' }));
+});
+
+const themeHelpers = glob.sync(`${baseDir}/helpers/**/*.js`);
+themeHelpers.forEach(file => {
+    const fileName = file.substring(file.lastIndexOf("/") + 1).replace('.js', '');
+    Handlebars.registerHelper(fileName, require(file));
 });
 
 
 const gallery = require(`${__dirname}/data/thumbsup_config`);
-const template = Handlebars.compile(fs.readFileSync(`${baseDir}/index.hbs`,  {encoding: 'utf8'}));
-fs.writeFileSync(`${baseOutDir}/index.html`, template({gallery}), {encoding: 'utf8'});
+const template = Handlebars.compile(fs.readFileSync(`${baseDir}/index.hbs`, { encoding: 'utf8' }));
+fs.writeFileSync(`${baseOutDir}/index.html`, template({ gallery }), { encoding: 'utf8' });
 
 gm(`${baseDir}/background.jpg`)
     .resize(2000, 2000)
